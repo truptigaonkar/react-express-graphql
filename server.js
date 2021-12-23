3320b3943fe37ae0886459fa700602f8ecfa41f4
truptigaonkar <trups11.trupti@gmail.com>
2021-12-23 18:54:52 +0100

    Basic graphql example

--- server.js
+++ server.js
@@ -1,6 +1,46 @@
-const express = require('express')
+const express = require("express");
+const { buildSchema } = require("graphql");
+const { graphqlHTTP } = require("express-graphql");
+
 const app = express();
-app.get("/", (req, res) =>{
-    res.send("Up and running with graphql")
-})
-app.listen(5000, () => console.log('Server Running on port 5000'))
+
+const schema = buildSchema(`
+    type User {
+        name: String
+        age: Int
+        college: String
+    }
+    type Query {
+        hello: String
+        welcomeMessage(name: String, dayOfWeek: String): String
+        getUSer: User
+    }
+`);
+// Resolver
+const root = {
+  hello: () => {
+    return "hello world";
+  },
+  welcomeMessage: (args) => {
+    return `Hey, ${args.name} Hows life. Today is ${args.dayOfWeek}`;
+  },
+  getUSer: () => {
+    const user = {
+      name: "John Doe",
+      age: 26,
+      college: "Ruparel",
+    };
+    return user;
+  },
+};
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    graphiql: true,
+    schema: schema,
+    rootValue: root,
+  })
+); // http://localhost:4000/graphql
+
+app.listen(1000, () => console.log(`Server on port 1000`));
